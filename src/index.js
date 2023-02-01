e60551a971ac3e35fd1c93ce8b12580216fd2f79
Sirage-t <Sirage.tr@gmail.com>
2023-02-01 21:20:06 +1030

    initial

---
+++ src/index.js
@@ -0,0 +1,333 @@
+#!/usr/bin/env node
+
+import { appendFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import { outputJson, pathExists, readJson } from 'fs-extra/esm';
+import { PythonShell } from 'python-shell';
+
+import chalk from 'chalk';
+import ora from 'ora';
+import { search } from 'fast-fuzzy';
+import inquirer from 'inquirer';
+import fuzzyPath from 'inquirer-fuzzy-path';
+import autoComplete from 'inquirer-autocomplete-prompt';
+
+import {
+  getDirectoriesRecursive,
+  delay,
+  isEmpty,
+  containsWhitespace,
+  getCurrentDirsOnce,
+} from './utilities.js';
+
+//  Register InquirerJS plugins
+inquirer.registerPrompt('fuzzypath', fuzzyPath);
+inquirer.registerPrompt('autocomplete', autoComplete);
+
+// Current directory
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const __cwd = process.cwd();
+const log = console.log;
+
+const configFileName = 'dll.config.json';
+const pathToConfigFile = path.join(__cwd, 'dll_config');
+const configFileFullPath = path.join(pathToConfigFile, configFileName);
+
+const configFileExists = await pathExists(configFileFullPath);
+
+try {
+  const ddir = await getDirectoriesRecursive(__cwd, {
+    baseOnly: false,
+    includeCurrentDir: true,
+  });
+  log(ddir);
+} catch (error) {
+  log(error);
+}
+
+// grab api key and project names from the config file
+let jsonProjectNames, jsonApiKeyNames, jsonApiKeys;
+if (configFileExists) {
+  try {
+    const configFileJson = await readJson(configFileFullPath);
+    jsonProjectNames = configFileJson.DA_playground_projects;
+    jsonApiKeys = new Map(Object.entries(configFileJson.API_keys));
+    jsonApiKeyNames = Array.from(jsonApiKeys.keys());
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Create a configuration file for the current project if wanted
+let answers_CreateConfig;
+if (!configFileExists) {
+  answers_CreateConfig = await inquirer.prompt([
+    {
+      name: 'wantToCreateConfigFile',
+      message: `We couldn't locate a ${chalk.yellowBright.bold(
+        configFileName
+      )} file for this project, would you like to create one?`,
+      type: 'confirm',
+    },
+    {
+      name: 'API_key',
+      message: `What is the Docassemble API key? ${chalk.italic('(see)')}`,
+      validate(_value) {
+        return isEmpty(_value)
+          ? chalk.yellowBright('API key cannot be empty')
+          : true;
+      },
+      when(_answersHash) {
+        return _answersHash.wantToCreateConfigFile;
+      },
+    },
+    {
+      name: 'API_key_name',
+      message: `What would you like to call this API key?`,
+      default: 'dll_api_key',
+      when(_answersHash) {
+        return _answersHash.wantToCreateConfigFile;
+      },
+    },
+    {
+      name: 'API_root',
+      message: `What is the API root url?`,
+      default: 'https://llaw3301.achelp.net/api',
+      when(_answersHash) {
+        return _answersHash.wantToCreateConfigFile;
+      },
+    },
+    {
+      name: 'DA_playground_project',
+      message: 'What is the name of your DA playground project?',
+      when(_answersHash) {
+        return _answersHash.wantToCreateConfigFile;
+      },
+      type: 'autocomplete',
+      suggestOnly: true,
+      emptyText: 'Searching for suggestions as you type (Tab to select)',
+      source: (_, input) => {
+        if (isEmpty(input)) return [];
+
+        return new Promise(async (resolve) => {
+          const _currentDir = await getCurrentDirsOnce(__cwd, {
+            excludePath: (nodePath) => nodePath == 'node_modules',
+            type: 'directory',
+            baseOnly: 'true',
+          });
+          const fuzzySearchResult = search(input, _currentDir);
+          resolve(fuzzySearchResult);
+        });
+      },
+      validate(_value) {
+        if (!/^(?:[a-z]|[A-Z]|-|[0-9])+$/.test(_value))
+          return chalk.yellowBright(
+            'Project name must only contain letters, numbers, or hyphens, without any space'
+          );
+
+        if (isEmpty(_value))
+          return chalk.yellowBright('Project name cannot be empty');
+
+        return true;
+      },
+    },
+    {
+      name: 'configFileLocation',
+      message: 'Where would you like to save your configuration file?',
+      type: 'autocomplete',
+      suggestOnly: false,
+      when(_answersHash) {
+        return _answersHash.wantToCreateConfigFile;
+      },
+      source: () => {
+        return getDirectoriesRecursive(__cwd, {
+          type: 'directory',
+          includeCurrentDir: false,
+          depthLimit: 3,
+          currentDirText: `Current Directory [${__cwd}]`,
+        });
+      },
+    },
+  ]);
+
+  if (answers_CreateConfig.wantToCreateConfigFile) {
+    const spinner = ora(
+      `Creating file ${chalk.blue("'dll.config.json'")} `
+    ).start();
+
+    let constructedConfig = {
+      API_keys: {},
+      DA_playground_projects: [answers_CreateConfig.DA_playground_project],
+    };
+
+    constructedConfig.API_keys[answers_CreateConfig.API_key_name] = {
+      api_key: answers_CreateConfig.API_key,
+      api_root: answers_CreateConfig.API_root,
+    };
+
+    try {
+      // TODO: currently it create config file in the current node directory, add option to select save location (current dir or parent dir) - the question already exists but not implemented below
+      await outputJson(configFileFullPath, constructedConfig, { spaces: 2 });
+
+      // add a .gitignore file so that the api key is not accidentally pushed to GitHub
+      // TODO: don't add if line already exists in file
+      await appendFile(
+        path.join(__cwd, '.gitignore'),
+        '\n# Digital Law Lab Config\ndll_config/**'
+      );
+
+      await delay(300);
+      spinner.succeed(chalk.greenBright('Config file created successfully'));
+    } catch (error) {
+      spinner.fail(chalk.redBright('Failed to create config file'));
+      console.error(error);
+      process.exit(1);
+    }
+    await delay(100);
+  }
+}
+
+let questions_PushToDA = [
+  {
+    name: 'folderPath',
+    message: 'Which folder do you want to push to the playground?',
+    type: 'autocomplete',
+    suggestOnly: false,
+    source: () => {
+      return getDirectoriesRecursive(__cwd, {
+        excludePath: (nodePath) => nodePath == 'node_modules',
+        includeCurrentDir: true,
+        currentDirText: `Current folder [${__cwd}]`,
+      });
+    },
+    filter(input) {
+      if (input.includes('Current folder'))
+        return String(input.split(/\[|\]/)[1]);
+      return path.join(__cwd, input);
+    },
+  },
+  configFileExists && {
+    name: 'playgroundProject',
+    message: 'Which playground project do you want to push your code to?',
+    type: 'list',
+    type: 'autocomplete',
+    suggestOnly: false,
+    source: () => Promise.resolve([...jsonProjectNames, 'Something else']),
+  },
+  {
+    name: 'customPlaygroundProject',
+    message: 'Please type the name of the playground project:',
+    when: (_answersHash) => {
+      return (
+        _answersHash.playgroundProject == 'Something else' ||
+        (!answers_CreateConfig?.wantToCreateConfigFile &&
+          typeof _answersHash.playgroundProject === 'undefined')
+      );
+    },
+    type: 'autocomplete',
+    suggestOnly: true,
+    emptyText: 'Searching for suggestions as you type (Tab to select)',
+    source: (_, input) => {
+      if (isEmpty(input)) return [];
+
+      return new Promise(async (resolve) => {
+        const _currentDir = await getCurrentDirsOnce(__cwd, {
+          excludePath: (nodePath) => nodePath == 'node_modules',
+          type: 'directory',
+          baseOnly: 'true',
+        });
+        const fuzzySearchResult = search(input, _currentDir);
+        resolve(fuzzySearchResult);
+      });
+    },
+    validate(_value) {
+      if (!/^(?:[a-z]|[A-Z]|-|[0-9])+$/.test(_value))
+        return chalk.yellowBright(
+          'Project name must only contain letters, numbers, or hyphens, without any space'
+        );
+
+      if (isEmpty(_value))
+        return chalk.yellowBright('Project name cannot be empty');
+
+      return true;
+    },
+  },
+  configFileExists && {
+    name: 'apiKeyName',
+    message: `You have more than one API key name in your config file, which one would you like to use?`,
+    type: 'list',
+    choices: () => jsonApiKeyNames,
+    when() {
+      return typeof jsonApiKeyNames !== 'string' && jsonApiKeyNames.length > 1;
+    },
+  },
+  configFileExists && {
+    name: 'apiKeyNameConfirm',
+    message: `Do you want to use ${chalk.yellow(
+      jsonApiKeyNames[0]
+    )} as your API key`,
+    type: 'confirm',
+    when() {
+      return typeof jsonApiKeyNames === 'string' || jsonApiKeyNames.length == 1;
+    },
+  },
+  !answers_CreateConfig?.wantToCreateConfigFile && {
+    name: 'apiKey',
+    message: `What is the API key? (not the name)`,
+    when(_answersHash) {
+      return !_answersHash.apiKeyNameConfirm && !_answersHash.apiKeyName;
+    },
+    validate(_value) {
+      if (isEmpty(_value)) return chalk.yellowBright('API key cannot be empty');
+      if (containsWhitespace(_value))
+        return chalk.yellowBright(
+          'API key must not contain any whitespace character'
+        );
+      return true;
+    },
+  },
+  !answers_CreateConfig?.wantToCreateConfigFile && {
+    name: 'apiRoot',
+    message: 'What is the API root url?',
+    default: 'https://llaw3301.achelp.net/api',
+    when(_answersHash) {
+      return !_answersHash.apiKeyNameConfirm && !_answersHash.apiKeyName;
+    },
+  },
+].filter(Boolean);
+
+const answers_PushToDA = await inquirer.prompt(questions_PushToDA);
+
+if (answers_PushToDA.apiKey) {
+  // addApiKey(apiName, apiKey, apiRoot, apiObject) -> modified object
+  // modifyJsonFile(filePath, modifiedObject )
+}
+
+// user want to use api key found first in the configuration
+if (answers_PushToDA.apiKeyNameConfirm) {
+  answers_PushToDA.apiKeyName = jsonApiKeyNames[0];
+}
+// get project name from the configuration the user just created
+if (!!answers_CreateConfig) {
+  answers_PushToDA.playgroundProject =
+    answers_CreateConfig.DA_playground_project;
+}
+
+// run the puthon script to push to the playground
+const filesToPush = await getDirectoriesRecursive(answers_PushToDA.folderPath, {
+  type: 'both',
+});
+
+const spinnerPushing = ora('Pushing files').start();
+
+for (const file of filesToPush) {
+  await delay(100);
+  spinnerPushing.text = `Pushing ${file}`;
+}
+spinnerPushing.succeed('Files pushed successfully!');
+
+PythonShell.runString('x=1+1;print(x)', null, function (err) {
+  if (err) throw err;
+  console.log('finished');
+});
---
+++ src/utilities.js
@@ -0,0 +1,228 @@
+import { lstat, readdir, access, mkdir, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+
+/**
+ *
+ * @param {string} source
+ * @param {boolean} includeFiles
+ * @param {boolean} withFileTypes - if false the function will return `string[]` with the file/folder name, otherwise will return `Object[]` with methods like `isDirectory()`.
+ * @returns An array of directories within `source` (no subdirectories are traversed), will include files if `includeFiles = true`.
+ */
+export const getDirectories = async (
+  source,
+  includeFiles = false,
+  withFileTypes = false
+) => {
+  if ((await lstat(source)).isFile()) return [];
+  try {
+    return (await readdir(source, { withFileTypes: true }))
+      .filter((dirent) =>
+        !includeFiles
+          ? dirent.isDirectory()
+          : dirent.isFile() || dirent.isDirectory()
+      )
+      .map((dirent) => (withFileTypes ? dirent : dirent.name));
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+/**
+ *
+ * @param {string} source required
+ * @param {Object} options
+ * @param {'file'|'directory'|'both'} options.type whether to show only files, only directories, or both
+ * @param {boolean} options.baseOnly if true, will only return base name instead of the path i.e. `folderName` instead of `usr\src\folderName`
+ * @param {number} options.depthLimit how deep should the scan be into the subdirectories. Leave as undefined to scan all subdirectories.
+ * @param {function} options.excludePath A function that recieves a path and must return a boolean (true to exclude, false to keep)
+ * @param {boolean} options.includeCurrentDir whether to show the root directory (the source)
+ * @param {string} options.currentDirText what to show as the name of the root directory default is `'.'`.
+ * @param {string[]} foundDirList
+ * @param {string[]} remainingDirsToSearch
+ * @returns An array of directories and subdirectories. It will include files if `type = 'file' or 'all'`.
+ */
+export const getDirectoriesRecursive = async (
+  source,
+  options,
+  foundDirList = [],
+  remainingDirsToSearch = [],
+  rootPath,
+  level = 1
+) => {
+  try {
+    // set default options
+    options = {
+      ...{
+        type: 'directory',
+        baseOnly: false,
+        depthLimit: 3,
+        excludePath: (_nodePath) =>
+          _nodePath.includes('node_modules') || _nodePath.includes('.git'),
+        includeCurrentDir: false,
+        currentDirText: '.',
+      },
+      ...options,
+    };
+    if (!rootPath) rootPath = source;
+
+    console.log('source', source);
+
+    const showFiles = options.type == 'file' || options.type == 'both';
+
+    const dontShowDirectories = options.type == 'file';
+
+    const getCurrentDirectoryToScan = (_directoryList) => {
+      let _dirPath, _isDirectory, _depth;
+      if (typeof _directoryList[0] === 'string') {
+        _dirPath = path.join(source, _directoryList[0]);
+        _isDirectory = undefined;
+        _depth = undefined;
+      }
+
+      if (typeof _directoryList[0] === 'object') {
+        // the directory could either be `dirent` object returned by readdir or the object inside `remainingDirsToSearch`.
+        // if dirent the path is `source` + base name
+        // otherwise it is the original path preformated in `remainingDirsToSearch`.
+        _dirPath = !!_directoryList[0].name
+          ? path.join(source, _directoryList[0].name)
+          : path.join(rootPath, _directoryList[0].path);
+        // if dirent use the builtin method `isDirectory()`
+        _isDirectory = !!_directoryList[0].name
+          ? _directoryList[0].isDirectory()
+          : _directoryList[0].isDirectory;
+        _depth = !_directoryList[0].name
+          ? _directoryList[0].path.split(path.sep).length == 0
+            ? 1
+            : _directoryList[0].path.split(path.sep).length
+          : undefined;
+      }
+
+      return {
+        path: path.relative(rootPath, _dirPath),
+        isDirectory: _isDirectory,
+        level: _depth + 1,
+      };
+    };
+
+    const getFoundDirList = () => {
+      let _dirList = !options.baseOnly
+        ? foundDirList
+        : foundDirList.map((_path) => path.basename(_path));
+
+      if (options.includeCurrentDir)
+        _dirList = [options.currentDirText, ..._dirList];
+
+      return Promise.resolve(_dirList);
+    };
+
+    let dirs = await getDirectories(source, showFiles, showFiles);
+
+    console.log('dirs', dirs);
+
+    // depth level reached
+    if (options.depthLimit === level) {
+      foundDirList = [
+        ...foundDirList,
+        ...dirs
+          .map((base) =>
+            path.relative(
+              rootPath,
+              path.join(source, showFiles ? base.name : base)
+            )
+          )
+          .filter((_path) => !options.excludePath(_path)),
+      ];
+
+      dirs = [];
+    }
+
+    // current dir is empty and no remaining dirs to scan
+    if (dirs.length === 0 && remainingDirsToSearch.length === 0)
+      return getFoundDirList();
+
+    const currentDirList = dirs.length === 0 ? remainingDirsToSearch : dirs;
+    let currentDirToScan = getCurrentDirectoryToScan(currentDirList);
+    if (dirs.length !== 0) {
+      level += 1;
+    } else {
+      level = currentDirToScan.level;
+    }
+
+    // check if current dir is excluded
+    while (options.excludePath(currentDirToScan.path)) {
+      // current dir is excluded and no other dir exists to scan
+      if (currentDirList.length === 1) return getFoundDirList();
+      currentDirList.shift();
+      currentDirToScan = getCurrentDirectoryToScan(currentDirList);
+      console.log('path', currentDirToScan.path);
+    }
+
+    const subPath = path.join(rootPath, currentDirToScan.path);
+
+    // don't include the path if it is a directory and `dontShowDirectories` and `showFiles` are true.
+    foundDirList = [
+      ...foundDirList,
+      ...(dontShowDirectories && showFiles && currentDirToScan.isDirectory
+        ? []
+        : [currentDirToScan.path]),
+    ];
+
+    // if current directory is empty then we are scaning the remaining dirs.
+    if (dirs.length === 0) {
+      remainingDirsToSearch = remainingDirsToSearch.slice(1);
+    } else {
+      remainingDirsToSearch = [
+        ...dirs.slice(1).map((base) => ({
+          path: path.relative(
+            rootPath,
+            path.join(source, showFiles ? base.name : base)
+          ),
+          isDirectory: showFiles ? base.isDirectory() : undefined,
+        })),
+        ...remainingDirsToSearch,
+      ];
+    }
+
+    return await getDirectoriesRecursive(
+      subPath,
+      options,
+      foundDirList,
+      remainingDirsToSearch,
+      rootPath,
+      level
+    );
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+let alreadyVisitedSource, cachedDirs;
+export const getCurrentDirsOnce = async (source, options) => {
+  if (alreadyVisitedSource != source) {
+    alreadyVisitedSource = source;
+    cachedDirs = await getDirectoriesRecursive(source, options);
+  }
+  return Promise.resolve(cachedDirs);
+};
+
+export const delay = (duration = 1000) =>
+  new Promise((resolve) => setTimeout(resolve, duration));
+
+export const doesDirExists = (_path) =>
+  access(_path)
+    .then(() => true)
+    .catch(() => false);
+
+export const createFile = async (fileName, content, location) => {
+  const _path = path.join(location, fileName);
+  if (!(await doesDirExists(location))) {
+    await mkdir(location);
+  }
+  return writeFile(_path, content, 'utf-8');
+};
+
+export const isEmpty = (_value) => !_value || /^\s*$/.test(_value);
+
+export const containsWhitespace = (_value) => /\s+/.test(_value);
